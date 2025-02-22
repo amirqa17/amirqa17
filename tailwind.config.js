f64d0127d0be31c2ed3bd7927ce8e51fc5ad1443
Amir Ibraimov <ibraimov.amir1996@gmail.com>
2025-02-22 11:34:30 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
-module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        background: {
-          DEFAULT: '#18181B', // zinc-900
-          light: '#FAFAFA'    // zinc-50
-        },
-        text: {
-          DEFAULT: '#FAFAFA', // zinc-50
-          light: '#18181B'    // zinc-900
-        }
-      }
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 } 
